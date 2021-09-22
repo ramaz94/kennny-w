b3b83585780fdca0a5f5789c38d5688805e0eeea
ramaz94 <79368327+ramaz94@users.noreply.github.com>
2021-09-22 15:33:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+  $('.header__slider-inner').slick({
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow-prew" type="button"><img src="./img/arrow-prew.svg"></button>',
+    nextArrow: '<button class="slick-arrow-next" type="button"><img src="./img/arrow-next.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  
+
+});
+
+
+
